4d56f6e50c41a7915321e3be36eff05ffe3132c8
Sara Ocasar <sara.ocasar@ikasle.egibide.org>
2024-10-20 14:07:33 +0200

    Movimiento jugador

--- Assets/Code/PlayerManager.cs
+++ Assets/Code/PlayerManager.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Referencia al Input Action Asset
+    private CustomControls customControls;
+
+    private Vector2 moveInput;
+    private Rigidbody rb;
+    public float speed = 5f;
+    public float jumpForce = 5f;
+    private bool isGrounded;
+
+    private void Awake()
     {
-        
+        // Inicializar el Input Action Asset
+        customControls = new CustomControls();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        // Activar el Action Map y registrar las acciones
+        customControls.PlayerInput.Enable();
+        customControls.PlayerInput.Movement.performed += OnMove;
+        customControls.PlayerInput.Movement.canceled += OnMove;
+        customControls.PlayerInput.Jump.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        // Desactivar el Action Map y desregistrar las acciones
+        customControls.PlayerInput.Movement.performed -= OnMove;
+        customControls.PlayerInput.Movement.canceled -= OnMove;
+        customControls.PlayerInput.Jump.performed -= OnJump;
+        customControls.PlayerInput.Disable();
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Movimiento horizontal
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        // Capturar el input de movimiento
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        // Saltar si está en el suelo
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
